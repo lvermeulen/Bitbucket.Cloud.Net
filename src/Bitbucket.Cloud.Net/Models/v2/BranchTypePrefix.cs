fbe421fceedb81dda7e0093b162777afdcf79fa2
Bob343 <Zach.vampola@Gmail.com>
2020-10-24 03:52:14 -0400

    Fix branch model settings not posting correct format + response not deserializing (#10)

--- src/Bitbucket.Cloud.Net/Models/v2/BranchTypePrefix.cs
+++ src/Bitbucket.Cloud.Net/Models/v2/BranchTypePrefix.cs
@@ -1,7 +1,11 @@
-﻿namespace Bitbucket.Cloud.Net.Models.v2
+﻿using Bitbucket.Cloud.Net.Common.Converters;
+using Newtonsoft.Json;
+
+namespace Bitbucket.Cloud.Net.Models.v2
 {
 	public class BranchTypeInfo
 	{
+		[JsonConverter(typeof(BranchTypesConverter))]
 		public BranchTypes Kind { get; set; }
 		public string Prefix { get; set; }
 		public bool? Enabled { get; set; }
